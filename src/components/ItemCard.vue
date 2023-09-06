f8907923d09be9fd13cc163011c8efa9802b85cd
Coolacho <sofiaangel35@gmail.com>
2023-09-06 23:20:23 +0300

    Separated the normal and expanded item cards in different components

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -1,12 +1,11 @@
 <!--suppress CssUnusedSymbol -->
 <script setup>
 
-import {ref} from "vue";
 import {hoveredStore} from "@/store";
-import ItemDescriptionBox from "@/components/ItemDescriptionBox.vue";
 import ItemPrice from "@/components/ItemPrice.vue";
+import ItemCardExpanded from "@/components/ItemCardExpanded.vue";
 
-defineProps({
+const props = defineProps({
   item: {
     type: Object,
     required: true,
@@ -25,29 +24,15 @@ defineProps({
           && (typeof value.discount === "number" && (value.discount >= 0 && value.discount <=100))
           && (typeof value.title === "string")
           && (typeof value.description === "string")
-          && (value.images.every(img => {return typeof img[0] === "number" && typeof img[1] === "string"})
-          && (value.categories.every(cat => {return typeof cat[0] === "number" && typeof cat[1] === "string"})))
+          && (value.images.every(img => {return typeof img.id === "number" && typeof img.path === "string"})
+          && (value.categories.every(cat => {return typeof cat.id === "number" && typeof cat.name === "string"})))
     }*/
   }
 })
 
-const store = hoveredStore();
-const imageId = ref(0);
-function changeImgForward(images) {
-  if (imageId.value < images.length-1) {
-    setTimeout(() => {
-      ++imageId.value;
-    }, 2000);
-  }
-}
+provide('description', props.item["description"])
 
-function changeImgBackward() {
-  if (imageId.value > 0) {
-    setTimeout(() => {
-      --imageId.value;
-    }, 2000);
-  }
-}
+const store = hoveredStore();
 
 </script>
 
@@ -96,43 +81,11 @@ function changeImgBackward() {
   <!--and add a <Transition> component for the v-show which will have the enter and leave animations-->
   
   <Transition name="expand">
-    <div
-        id="expanded-card-wrapper"
+    <ItemCardExpanded
         v-show="store.hovered === item.id"
         @mouseleave="store.unhover()"
     >
-      <div id="left-side-container">
-        <div id="expanded-image-wrapper">
-          <div
-              class="img-change-buttons left"
-              @click="changeImgBackward()"
-          >
-            <img
-                src="@/assets/left-arrow.png"
-                alt="Previous photo"
-            >
-          </div>
-          <div
-              class="img-change-buttons right"
-              @click="changeImgForward(item.images)"
-          >
-            <img
-                src="@/assets/left-arrow.png"
-                alt="Next photo"
-            >
-          </div>
-          <img
-              id="image-wheel"
-              :src="item.images[imageId].path"
-              :alt="item.name"
-          >
-        </div>
-        <h1>{{ item.name }}</h1>
-      </div>
-      <div id="right-side-container">
-        <ItemDescriptionBox :description = String(item.description)></ItemDescriptionBox>
-      </div>
-    </div>
+    </ItemCardExpanded>
   </Transition>
 
 </template>
@@ -204,120 +157,4 @@ function changeImgBackward() {
     }
   }
 
-  #expanded-card-wrapper {
-    width: 75%;
-    height: 80%;
-    border: solid 2px #3C4043;
-    border-radius: 12px;
-    z-index: 2;
-    position: absolute;
-    left: 12.5%;
-    top: 10%;
-    background-color: #3C4043;
-    display: grid;
-    grid-auto-columns: 50%;
-    grid-auto-flow: column;
-    padding: 50px;
-    box-sizing: border-box;
-  }
-  #expanded-card-wrapper h1 {
-    font-family: 'Belanosima', sans-serif;
-    font-size: 35px;
-    color: white;
-  }
-  #left-side-container, #right-side-container {
-    min-height: 0;
-  }
-
-  #expanded-image-wrapper {
-    width: 85%;
-    height: 65%;
-    position: relative;
-    overflow: hidden;
-  }
-
-  .img-change-buttons {
-    width: 30px;
-    height: 100%;
-    z-index: 3;
-    position: absolute;
-  }
-  .img-change-buttons.left {
-    left: 0;
-    border-top-left-radius: 10px;
-    border-bottom-left-radius: 10px;
-  }
-  .img-change-buttons.right {
-    right: 0;
-    border-top-right-radius: 10px;
-    border-bottom-right-radius: 10px;
-  }
-  .img-change-buttons:hover {
-    background-color: #80868B55;
-  }
-  .img-change-buttons:hover > *{
-    visibility: visible;
-  }
-  .img-change-buttons img {
-    width: 30px;
-    max-height: 30px;
-    position: absolute;
-    top: 45%;
-    visibility: hidden;
-  }
-  .img-change-buttons img::selection {
-    background: transparent;
-  }
-  .img-change-buttons.right img {
-    rotate: 180deg;
-  }
-
-  #image-wheel{
-    border-radius: 10px;
-    width: 100%;
-    height: 100%;
-  }
-
-  .exitImgLeft {
-    transform: scale3d(0.65, 0.65, 0.65);
-    translate: -500px;
-    transition:
-        transform 0.5s,
-        translate 0.5s 1s;
-  }
-  .enterImgRight {
-    translate: 500px;
-    transform: scale3d(1, 1, 1);
-    transition:
-        translate 0.5s,
-        transform 0.5s 1s;
-  }
-  .exitImgRight {
-    transform: scale3d(0.65, 0.65, 0.65);
-    translate: 500px;
-    transition:
-        transform 0.5s,
-        translate 0.5s 1s;
-  }
-  .enterImgLeft {
-    translate: -500px;
-    transform: scale3d(1, 1, 1);
-    transition:
-        translate 0.5s,
-        transform 0.5s 1s;
-  }
-
-  @media screen and (max-width: 400px) {
-    #expanded-card-wrapper {
-      padding: 20px;
-      grid-auto-flow: row;
-      grid-auto-rows: 50%;
-      grid-auto-columns: 100%;
-    }
-
-    #expanded-card-wrapper h1 {
-      font-size: 20px;
-    }
-
-  }
 </style>
---
+++ src/components/ItemCardExpanded.vue
@@ -0,0 +1,60 @@
+<script setup>
+
+import ItemDescriptionBox from "@/components/ItemDescriptionBox.vue";
+import ItemImageWheel from "@/components/ItemImageWheel.vue";
+</script>
+
+<template>
+  <div
+      id="expanded-card-wrapper"
+  >
+    <div id="left-side-container">
+      <ItemImageWheel :images = Array(item.images) :item-name = String(item.name)></ItemImageWheel>
+      <h1>{{ item.name }}</h1>
+    </div>
+    <div id="right-side-container">
+      <ItemDescriptionBox :description = String(item.description)></ItemDescriptionBox>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  #expanded-card-wrapper {
+    width: 75%;
+    height: 80%;
+    border: solid 2px #3C4043;
+    border-radius: 12px;
+    z-index: 2;
+    position: absolute;
+    left: 12.5%;
+    top: 10%;
+    background-color: #3C4043;
+    display: grid;
+    grid-auto-columns: 50%;
+    grid-auto-flow: column;
+    padding: 50px;
+    box-sizing: border-box;
+  }
+  #expanded-card-wrapper h1 {
+    font-family: 'Belanosima', sans-serif;
+    font-size: 35px;
+    color: white;
+  }
+  #left-side-container, #right-side-container {
+    min-height: 0;
+  }
+
+  @media screen and (max-width: 400px) {
+    #expanded-card-wrapper {
+      padding: 20px;
+      grid-auto-flow: row;
+      grid-auto-rows: 50%;
+      grid-auto-columns: 100%;
+    }
+
+    #expanded-card-wrapper h1 {
+      font-size: 20px;
+    }
+
+  }
+</style>
