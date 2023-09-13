7ec3051e966e81ec06f5e3e049e876848791f39f
Coolacho <sofiaangel35@gmail.com>
2023-09-13 23:41:53 +0300

    Separated the product grid in own component

---
+++ src/components/ProductGrid.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import ProductCard from "@/components/Product/ProductCard.vue";
+
+  defineProps(['products'])
+</script>
+
+<template>
+  <div>
+    <ProductCard v-for="item in products" :key="item.id" :product="item"></ProductCard>
+  </div>
+</template>
+
+<style scoped>
+  div {
+    display: grid;
+    grid-template-rows: repeat(auto-fit, minmax(480px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(275px, 1fr));
+    align-items: center;
+    justify-items: center;
+  }
+</style>
