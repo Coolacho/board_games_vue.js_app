ea14b28a7368132e84c685d2ab45e6285cb19eb2
Coolacho <sofiaangel35@gmail.com>
2023-09-13 23:41:31 +0300

    Created components for different pages

---
+++ src/components/PageBoardGamesAll.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+  import {ref} from "vue";
+  import ProductService from "@/services/ProductService";
+  import ProductGrid from "@/components/ProductGrid.vue";
+
+  const products = ref(null);
+  getAllProducts();
+
+  function getAllProducts() {
+    ProductService.getAllProducts().then((response) =>{
+      products.value = response.data;
+      console.log(products.value)
+    });
+  }
+
+</script>
+
+<template>
+  <ProductGrid :products="products"></ProductGrid>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/PageBoardGamesDiscounted.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+  import {ref} from "vue";
+  import ProductService from "@/services/ProductService";
+  import ProductGrid from "@/components/ProductGrid.vue";
+
+  const products = ref(null);
+  getDiscountedProducts();
+
+  function getDiscountedProducts() {
+    ProductService.getDiscountedProducts().then((response) =>{
+      products.value = response.data;
+      console.log(products.value)
+    });
+
+  }
+
+</script>
+
+<template>
+  <ProductGrid :products="products"></ProductGrid>
+</template>
+
+<style scoped>
+
+</style>
