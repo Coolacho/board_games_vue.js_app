25c4c0815c1329194f7ef5ba8009e53abf3fd4ef
Coolacho <sofiaangel35@gmail.com>
2024-03-04 01:42:54 +0200

    added basic behaviour

--- src/components/PageProductsAccessories.vue
+++ src/components/PageProductsAccessories.vue
@@ -1,11 +1,23 @@
 <script setup>
+import {ref} from "vue";
+import ProductService from "@/services/ProductService";
+import ProductGrid from "@/components/Product/ProductGrid.vue";
 
+const products = ref(null);
+getAccessories();
+
+function getAccessories() {
+  ProductService.getNewProducts().then((response) =>{
+    products.value = response.data;
+  });
+
+}
 </script>
 
 <template>
 
 </template>
-
+  <ProductGrid :products="products"></ProductGrid>
 <style scoped>
 
 </style>
